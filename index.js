bdc210a85ab825397b50604a8ebe09d593fb4552
EunJae <dldmswo1209@naver.com>
2022-10-31 00:02:04 +0900

    sec

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+
 app.listen(8080, function() {
     console.log('listening on 8080')
 })
 app.get('/', function(req, res) { 
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/mypage.html")
 })
 
 app.get('/home', function(req,res){
@@ -29,4 +31,68 @@ app.get('/fruit', function(req,res){
 
 app.get('/hallym', function(req,res){
     res.sendFile(__dirname + "/hallym.html")
+})
+
+app.get('/mypage', function(req,res){
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.get('/me/1', function(req, res){
+    fs.readFile('./images/me1.jpg', function(err, data){
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
+        res.end(data);
+    })
+})
+
+app.get('/me/2', function(req, res){
+    fs.readFile('./images/me2.jpg', function(err, data){
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
+        res.end(data);
+    })
+})
+
+app.get('/me/3', function(req, res){
+    fs.readFile('./images/me3.jpg', function(err, data){
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
+        res.end(data);
+    })
+})
+
+app.get('/me/4', function(req, res){
+    fs.readFile('./images/me4.jpg', function(err, data){
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
+        res.end(data);
+    })
+})
+
+app.get('/outside', function(req, res){
+    fs.readFile('./images/outside.jpg', function(err, data){
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
+        res.end(data);
+    })
+})
+
+app.get('/img/1', function(req, res){
+    fs.readFile('./images/img1.jpg', function(err, data){
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
+        res.end(data);
+    })
+})
+app.get('/img/2', function(req, res){
+    fs.readFile('./images/img2.jpg', function(err, data){
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
+        res.end(data);
+    })
+})
+app.get('/img/3', function(req, res){
+    fs.readFile('./images/img3.jpg', function(err, data){
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
+        res.end(data);
+    })
+})
+app.get('/img/4', function(req, res){
+    fs.readFile('./images/img4.jpg', function(err, data){
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
+        res.end(data);
+    })
 })
